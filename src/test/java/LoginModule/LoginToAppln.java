6908e96c2df4f51d6b96c3b6fcfb6be4137c65a3
Alpha-Team <Alpha-Team@DESKTOP-MV3N3RD>
2026-05-13 10:42:28 +0530

    sam commit

---
+++ src/test/java/LoginModule/LoginToAppln.java
@@ -0,0 +1,11 @@
+package LoginModule;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class LoginToAppln {
+	@Test
+	public void login() {
+		Reporter.log("login");
+	}
+}
