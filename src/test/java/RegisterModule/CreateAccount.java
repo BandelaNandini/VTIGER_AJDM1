6d243def5ad79d0374628b69ff515a5763b149e1
Alpha-Team <Alpha-Team@DESKTOP-MV3N3RD>
2026-05-13 10:43:25 +0530

    JOHN COMMIT

---
+++ src/test/java/RegisterModule/CreateAccount.java
@@ -0,0 +1,14 @@
+package RegisterModule;
+
+import org.junit.Test;
+import org.testng.Reporter;
+
+public class CreateAccount {
+
+	@Test
+	public void acct() {
+
+		Reporter.log("register");
+	}
+
+}
